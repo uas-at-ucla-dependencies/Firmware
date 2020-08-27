5b17960454ee0537e4f24fd5bc80fc8a17b1713d
Ryan Nemiroff <ryguyn@gmail.com>
2020-08-27 01:44:24 -0700

    Update sitl_gazebo submodule

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # Note: A lot of the important build/run stuff happens in Firmware/platforms/posix/cmake/sitl_target.cmake if you want to understand how it works
 git submodule update --init --recursive
 make px4_sitl_default px4
-make px4_sitl_default sitl_gazebo CPPFLAGS=-DTRUE=1
+make px4_sitl_default sitl_gazebo
